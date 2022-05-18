b94867baaee8615c8fc80df89a969ede033727f4
Jimmie Swaggerty <jimmie.swaggerty@gmail.com>
2022-05-18 12:46:26 -0500

    API Function fixes

--- constants/api-endpoints.js
+++ constants/api-endpoints.js
@@ -1,4 +1,11 @@
+let baseUrl = 'http://localhost:3300/'
 const APIEndPoints = {
-
+    user: {
+        addUser: baseUrl + '/user',
+        editUser: baseUrl + '/user/{userId}',
+        deleteUser: baseUrl + '/user/{userId}',
+        getAll: baseUrl + '/users',
+        getUserById: baseUrl + '/user/{userId}'
+    }
 }
 export default APIEndPoints
--- services/user-service.js
+++ services/user-service.js
@@ -1,4 +1,65 @@
+import axios from 'axios'
+import APIEndPoints from '../constants/api-endpoints'
 const UserService = {
-
+    addUser(reqObj) {
+        let url = APIEndPoints.user.addUser
+        return axios.post(url, reqObj)
+            .then(res => {
+                console.log(res)
+                return res
+            })
+            .catch(err => {
+                console.log(err)
+                return err
+            })
+    },
+    editUser(id, reqObj) {
+        let url = APIEndPoints.user.editUser.replace("{userId}", id)
+        return axios.put(url, reqObj)
+            .then(res => {
+                console.log(res)
+                return res
+            })
+            .catch(err => {
+                console.log(err)
+                return err
+            })
+    },
+    getUserById(id) {
+        let url = APIEndPoints.user.getUserById.replace("{userId}", id)
+        return axios.get(url)
+            .then(res => {
+                console.log(res)
+                return res
+            })
+            .catch(err => {
+                console.log(err)
+                return err
+            })
+    },
+    deleteUser(id) {
+        let url = APIEndPoints.user.deleteUser.replace("{userId}", id)
+        return axios.delete(url)
+            .then(res => {
+                console.log(res)
+                return res
+            })
+            .catch(err => {
+                console.log(err)
+                return err
+            })
+    },
+    getAll() {
+        let url = APIEndPoints.user.getAll
+        return axios.get(url)
+            .then(res => {
+                console.log(res)
+                return res
+            })
+            .catch(err => {
+                console.log(err)
+                return err
+            })
+    }
 }
 export default UserService
